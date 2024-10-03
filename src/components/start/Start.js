d0789c636fd314e4b298a584198a61422762ec46
VenkataSatyaSrikarParvatha <2200032432@kluniversity.in>
2024-10-03 10:27:22 +0530

    Updated Withdraw component with withdrawal history and added CSS

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Start.css'; // Make sure you create this CSS file for styling
+import logo from './1.png'; // Import the logo
 
 const Start = () => {
     return (
         <div className="start-container">
-            <img src="1.png" alt="Saahas Logo" className="logo" />
+            <img src={logo} alt="Saahas Logo" className="logo" />
             <h1>Welcome to Saahas</h1>
             <p>Your journey to support and resources begins here.</p>
             <Link to="/home">
